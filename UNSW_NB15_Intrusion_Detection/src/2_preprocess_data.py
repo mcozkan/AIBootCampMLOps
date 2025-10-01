bb0964ec8371b28837515bd2f92bf138ecdee96d
Murat Çağrı Özkan <mcagriozkann@gmail.com>
2025-10-01 12:42:44 +0300

    added files mcoz

--- UNSW_NB15_Intrusion_Detection/src/2_preprocess_data.py
+++ UNSW_NB15_Intrusion_Detection/src/2_preprocess_data.py
@@ -4,3 +4,22 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+
+def main():
+    with mlflow.start_run(run_name = "preprocess_data"):
+        print("=== 1. STEP : PREPROCESS DATA ===")
+        df = pd.read_csv("/data/raw/UNSW_NB15_training-set.csv")
+        df_num = df.select_dtypes(include = ['float', 'int'])
+
+
+        def compare(df1_num, df2_all):
+            number = []
+            new = []
+            for i in df1_num.columns:
+                number.append(i)
+            for j in df2_all.columns:
+                new.append(j)
+            if len(number) == len(new):
+            print("No categorical data dropped from the dataset!")
+        else:
+            print(f"New non-cat dataset features count : {len(new)}")
